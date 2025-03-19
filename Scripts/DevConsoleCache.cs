cd9960449ca0ae6dfcca7c77283a70b584f69c7c
Jerbo <60573821+tomjerbo@users.noreply.github.com>
2025-03-19 18:18:46 +0100

    Removed old code about caching scenes

--- Scripts/DevConsoleCache.cs
+++ Scripts/DevConsoleCache.cs
@@ -8,9 +8,6 @@ public class DevConsoleCache : ScriptableObject
 #if UNITY_EDITOR
     [UnityEditor.InitializeOnLoadMethod, DevCommand("LoadCache")]
     static void CacheAssetReferences() {
-        /*
-         * Cant use assetdatabase in builds, need a way to load/cache assets
-         */
         DevConsoleCache cache = Resources.Load<DevConsoleCache>(ASSET_PATH);
     
         
@@ -24,25 +21,6 @@ public class DevConsoleCache : ScriptableObject
             cache.AssetReferences[i] = UnityEditor.AssetDatabase.LoadAssetAtPath<ScriptableObject>(path);
             cache.AssetNames[i] = cache.AssetReferences[i].name;
         }
-        
-        
-
-        // assetGuids = UnityEditor.AssetDatabase.FindAssets("t:Scene", SEARCH_FOLDERS);
-        // cache.SceneNames = new string[assetGuids.Length];
-        // cache.ScenePaths = new string[assetGuids.Length];
-        //
-        // for (int i = 0; i < assetGuids.Length; i++) {
-        //     string guid = assetGuids[i];
-        //     string path = UnityEditor.AssetDatabase.GUIDToAssetPath(guid);
-        //     // Example path: Assets/Spawn Points/Map 1/Docks In Water.asset
-        //     //                              Split -> [ assetName.extension ]
-        //     //                              Split -> [ assetName ]
-        //
-        //     string nameFromPath = path.Split(PATH_SPLITTER)[^1]; // Last split is assetName.extension
-        //     string nameWithoutExtension = nameFromPath.Split(ASSET_EXTENSION_SPLITTER)[0]; // First split is assetName
-        //     cache.SceneNames[i] = nameWithoutExtension;
-        //     cache.ScenePaths[i] = path;
-        // }
 
         // Debug.Log($"DevConsole Cached -> {cache.AssetReferences.Length} ScriptableObjects & {cache.SceneNames.Length} Scenes");
     }
@@ -52,11 +30,5 @@ public class DevConsoleCache : ScriptableObject
     public const string ASSET_PATH = "Dev Console Cache";
     [HideInInspector] public ScriptableObject[] AssetReferences;
     [HideInInspector] public string[] AssetNames;
-    // [HideInInspector] public string[] SceneNames;
-    // [HideInInspector] public string[] ScenePaths;
-    
-    
-    // const char PATH_SPLITTER = '/';
-    // const char ASSET_EXTENSION_SPLITTER = '.';
 }
 }
