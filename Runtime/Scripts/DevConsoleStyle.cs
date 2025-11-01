6366cf9ebe7f5819c256baf4619e75e67970b1ca
Jerbo <60573821+tomjerbo@users.noreply.github.com>
2025-11-01 12:55:18 +0100

    testing define to use tabs from inspector

--- Runtime/Scripts/DevConsoleStyle.cs
+++ Runtime/Scripts/DevConsoleStyle.cs
@@ -3,12 +3,23 @@ using UnityEngine;
 namespace Jerbo.DevConsole {
     public class DevConsoleStyle : ScriptableObject {
         public const string BASE_ASSET_PATH = "Base_Dev Console Style";
-        
+
+#if JERBO_INSPECTOR
+        [Jerbo.Inspector.Tab("Core", Jerbo.Inspector.Tab.Color.Red)]
+#else
         [Header("Core")] 
+#endif
         public Texture2D ConsoleIcon;
+        public Vector2Int ConsoleIconFrames;
+        public float ConsolIconAnimSpeed;
         public GUISkin ConsoleSkin; 
             
+        
+#if JERBO_INSPECTOR
+        [Jerbo.Inspector.Tab("Console Colors", Jerbo.Inspector.Tab.Color.Pink)]
+#else
         [Header("Console Colors")]
+#endif
         public Color BackgroundColor = Color.white;
         public Color InputTextDefault = Color.white;
         public Color SelectedCommand = Color.white;
@@ -24,15 +35,21 @@ namespace Jerbo.DevConsole {
         public Color HintTextColorDefault = Color.white;
         public Color HintTextColorSelected = Color.white;
 
-        
+#if JERBO_INSPECTOR
+        [Jerbo.Inspector.Tab("Layout", Jerbo.Inspector.Tab.Color.Blue)]
+#else
         [Header("Layout")]
+#endif
         public float ConsoleTextSize = 36f;
         public float HintBoxBottomPadding = 6;
         public float HintBoxHeightOffset = 0;
         public int ConsoleIconSize = 26;
         
-        
+#if JERBO_INSPECTOR
+        [Jerbo.Inspector.Tab("Animations", Jerbo.Inspector.Tab.Color.Yellow)]
+#else
         [Header("Animations")]
+#endif
         public float SelectHintBumpOffsetAmount = 12f;
         public float SelectHintBumpSpeed = 8f;
         public float ArgHelpBumpOffsetAmount = 12f;
