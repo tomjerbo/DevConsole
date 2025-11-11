9e6c38f68ac4d4c298adbc332bf8b872b6329614
Jerbo <60573821+tomjerbo@users.noreply.github.com>
2025-11-11 18:57:41 +0100

    fix: serializing style asset

--- Runtime/Scripts/DevConsoleStyle.cs
+++ Runtime/Scripts/DevConsoleStyle.cs
@@ -1,61 +1,49 @@
 using UnityEngine;
 
 namespace Jerbo.DevConsole {
+    
+    [System.Serializable]
     public class DevConsoleStyle : ScriptableObject {
             
-#if JERBO_INSPECTOR
-        [Jerbo.Inspector.Tab("Core", Jerbo.Inspector.Tab.Color.Red)]
-#else
         [Header("Core")] 
-#endif
-        public Texture2D ConsoleIcon;
-        public Vector2Int ConsoleIconFrames = new (4,1);
-        public float ConsolIconAnimSpeed = 1f;
-        public GUISkin ConsoleSkin;
-        public bool keepConsoleOpenAfterCommand;
-        public KeyCode[] openConsoleKey;
+        [SerializeField] public Texture2D ConsoleIcon;
+        [SerializeField] public Vector2Int ConsoleIconFrames = new (4,1);
+        [SerializeField] public float ConsolIconAnimSpeed = 1f;
+        [SerializeField] public GUISkin ConsoleSkin;
+        [SerializeField] public bool keepConsoleOpenAfterCommand;
+        [SerializeField] public KeyCode[] openConsoleKey;
         
-#if JERBO_INSPECTOR
-        [Jerbo.Inspector.Tab("Console Colors", Jerbo.Inspector.Tab.Color.Pink)]
-#else
         [Header("Console Colors")]
-#endif
-        public Color BackgroundColor = Color.white;
-        public Color InputTextDefault = Color.white;
-        public Color SelectedCommand = Color.white;
-        public Color SelectedArgument = Color.white;
+        [SerializeField] public Color BackgroundColor = Color.white;
+        [SerializeField] public Color InputTextDefault = Color.white;
+        [SerializeField] public Color SelectedCommand = Color.white;
+        [SerializeField] public Color SelectedArgument = Color.white;
         
         [Space(12)]
-        public Color ValidCommand = Color.white;
-        public Color InputArgumentType = Color.white;
-        public Color InputArgumentTypeBorder = Color.white;
-        public Color RecordMacroColor = Color.white;
+        [SerializeField] public Color ValidCommand = Color.white;
+        [SerializeField] public Color InputArgumentType = Color.white;
+        [SerializeField] public Color InputArgumentTypeBorder = Color.white;
+        [SerializeField] public Color RecordMacroColor = Color.white;
         
         [Space(12)]
-        public Color HintTextColorDefault = Color.white;
-        public Color HintTextColorSelected = Color.white;
+        [SerializeField] public Color HintTextColorDefault = Color.white;
+        [SerializeField] public Color HintTextColorSelected = Color.white;
 
-#if JERBO_INSPECTOR
-        [Jerbo.Inspector.Tab("Layout", Jerbo.Inspector.Tab.Color.Blue)]
-#else
+
         [Header("Layout")]
-#endif
-        public float ConsoleTextSize = 36f;
-        public float HintBoxBottomPadding = 6;
-        public float HintBoxHeightOffset = 0;
-        public int ConsoleIconSize = 26;
+        [SerializeField] public float ConsoleTextSize = 36f;
+        [SerializeField] public float HintBoxBottomPadding = 6;
+        [SerializeField] public float HintBoxHeightOffset = 0;
+        [SerializeField] public int ConsoleIconSize = 26;
         
-#if JERBO_INSPECTOR
-        [Jerbo.Inspector.Tab("Animations", Jerbo.Inspector.Tab.Color.Yellow)]
-#else
+        
         [Header("Animations")]
-#endif
-        public float SelectHintBumpOffsetAmount = 12f;
-        public float SelectHintBumpSpeed = 8f;
-        public float ArgHelpBumpOffsetAmount = 12f;
-        public float ArgHelpBumpSpeed = 8f;
-        public float ArgHelpWidthPadding = 8f;
-        public AnimationCurve SelectionBumpCurve;
-        public AnimationCurve ArgumentTypeBumpCurve;
+        [SerializeField] public float SelectHintBumpOffsetAmount = 12f;
+        [SerializeField] public float SelectHintBumpSpeed = 8f;
+        [SerializeField] public float ArgHelpBumpOffsetAmount = 12f;
+        [SerializeField] public float ArgHelpBumpSpeed = 8f;
+        [SerializeField] public float ArgHelpWidthPadding = 8f;
+        [SerializeField] public AnimationCurve SelectionBumpCurve;
+        [SerializeField] public AnimationCurve ArgumentTypeBumpCurve;
     }
 }
