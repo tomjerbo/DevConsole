591bd6b62584fcdb0364eaabb73d268cbb031cdf
Jerbo <tomjerbo@users.noreply.github.com>
2026-08-19 16:33:39 +0200

    fix: setting default values to avoid having to copy the asset from packages

--- Editor/CreatePluginAssets.cs
+++ Editor/CreatePluginAssets.cs
@@ -4,55 +4,54 @@ using UnityEngine;
 namespace Jerbo.DevConsole {
 public static class CreatePluginAssets
 {
-    [UnityEditor.InitializeOnLoadMethod, UnityEditor.InitializeOnEnterPlayMode]
-    static async void CreatePackageFolder() {
+    
+    [UnityEditor.InitializeOnLoadMethod]
+    static async void create_package_folder() {
         
         if (System.IO.Directory.Exists(DevConsole.PLUGINS_FOLDER_PATH) == false) {
             System.IO.Directory.CreateDirectory(DevConsole.PLUGINS_FOLDER_PATH);
         }
-        bool shouldSaveAssets = false;
+        bool should_save_assets = false;
 
         await Task.Delay(100); // Fixes some strange issue where it doesn't find the assets when importing the package..
         
         /*
          * Cache
          */
-        DevConsoleCache consoleCache = UnityEditor.AssetDatabase.LoadAssetAtPath<DevConsoleCache>(DevConsole.DEV_CONSOLE_CACHE_PATH);
-        if (consoleCache == null) {
+        DevConsoleCache console_cache = UnityEditor.AssetDatabase.LoadAssetAtPath<DevConsoleCache>(DevConsole.DEV_CONSOLE_CACHE_PATH);
+        if (console_cache == null) {
             Debug.Log($"Could not find {nameof(DevConsoleCache)} at path '{DevConsole.DEV_CONSOLE_CACHE_PATH}'! Creating new.");
             
-            consoleCache = ScriptableObject.CreateInstance<DevConsoleCache>();
-            consoleCache.name = nameof(DevConsoleCache);
-            UnityEditor.AssetDatabase.CreateAsset(consoleCache, DevConsole.DEV_CONSOLE_CACHE_PATH);
-            consoleCache.RebuildCache_Editor();
-            shouldSaveAssets = true;
+            console_cache = ScriptableObject.CreateInstance<DevConsoleCache>();
+            console_cache.name = nameof(DevConsoleCache);
+            UnityEditor.AssetDatabase.CreateAsset(console_cache, DevConsole.DEV_CONSOLE_CACHE_PATH);
+            console_cache.RebuildCache_Editor();
+            should_save_assets = true;
         }
         
         /*
          * Style & Skin
          */
-        DevConsoleStyle consoleStyle = UnityEditor.AssetDatabase.LoadAssetAtPath<DevConsoleStyle>(DevConsole.DEV_CONSOLE_STYLE_PATH);
-        if (consoleStyle == null) {
+        DevConsoleStyle console_style = UnityEditor.AssetDatabase.LoadAssetAtPath<DevConsoleStyle>(DevConsole.DEV_CONSOLE_STYLE_PATH);
+        if (console_style == null) {
             Debug.Log($"Could not find {nameof(DevConsoleStyle)} at path '{DevConsole.DEV_CONSOLE_STYLE_PATH}'! Creating new.");
             
-            GUISkin baseGuiSkin = Resources.Load<GUISkin>("Base_Dev Console Skin");
-            GUISkin newSkin = Object.Instantiate(baseGuiSkin);
-            newSkin.name = "DevConsoleSkin";
-            UnityEditor.AssetDatabase.CreateAsset(newSkin, DevConsole.DEV_CONSOLE_SKIN_PATH);
+            GUISkin base_gui_skin = Resources.Load<GUISkin>("Base_Dev Console Skin");
+            GUISkin new_skin = Object.Instantiate(base_gui_skin);
+            new_skin.name = "DevConsoleSkin";
+            UnityEditor.AssetDatabase.CreateAsset(new_skin, DevConsole.DEV_CONSOLE_SKIN_PATH);
             
             
-            DevConsoleStyle baseStyleAsset = Resources.Load<DevConsoleStyle>("Base_Dev Console Style");
-            DevConsoleStyle newStyle = Object.Instantiate(baseStyleAsset);
-            consoleStyle = newStyle;
-            consoleStyle.name = nameof(DevConsoleStyle);
-            consoleStyle.console_skin = newSkin;
-            UnityEditor.AssetDatabase.CreateAsset(consoleStyle, DevConsole.DEV_CONSOLE_STYLE_PATH);
+            console_style = ScriptableObject.CreateInstance<DevConsoleStyle>();
+            console_style.name = nameof(DevConsoleStyle);
+            console_style.console_skin = new_skin;
+            UnityEditor.AssetDatabase.CreateAsset(console_style, DevConsole.DEV_CONSOLE_STYLE_PATH);
             
-            shouldSaveAssets = true;
+            should_save_assets = true;
         }
         
 
-        if (shouldSaveAssets) {
+        if (should_save_assets) {
             UnityEditor.AssetDatabase.SaveAssets();
         }
     }
--- Runtime/Scripts/DevConsoleStyle.cs
+++ Runtime/Scripts/DevConsoleStyle.cs
@@ -7,28 +7,27 @@ namespace Jerbo.DevConsole {
         
         [Header("Core")] 
         [SerializeField] public GUISkin console_skin;
-        [SerializeField] public KeyCode[] open_console_key;
+        [SerializeField] public KeyCode[] open_console_key = { KeyCode.T };
         // [SerializeField] public Texture2D console_icon;
         // [SerializeField] public Vector2Int console_icon_frames = new (4,1);
         // [SerializeField] public float console_icon_anim_speed = 1f;
         // [SerializeField] public bool show_icon = true;
         
-        
         [Header("Console Colors")]
-        [SerializeField] public Color color_background = Color.white;
-        [SerializeField] public Color color_text_default = Color.white;
-        [SerializeField] public Color color_text_selected = Color.white;
-        [SerializeField] public Color color_text_valid_cmd = Color.white;
-        [SerializeField] public Color color_outline_macro = Color.white;
+        [SerializeField] public Color color_background     = new (0.2561905f, 0.3f, 0.18f, 1f);
+        [SerializeField] public Color color_text_default   = new (0.5660378f, 0.5660378f, 0.5660378f, 1f);
+        [SerializeField] public Color color_text_selected  = new (1f, 0.8588235f, 0.3333333f, 1f);
+        [SerializeField] public Color color_text_valid_cmd = new (1f, 1f, 1f, 1f);
+        [SerializeField] public Color color_outline_macro  = new (1f, 0.3f, 0f, 1f);
         
         [Header("Layout")]
-        [SerializeField] public float console_text_size = 26f;
+        [SerializeField] public float console_text_size = 36f;
         // [SerializeField] public int console_icon_size = 26;
         
         
         [Header("Animations")]
         [SerializeField] public float select_hint_bump_offset_amount = 6f;
-        [SerializeField] public float select_hint_bump_speed = 12f;
+        [SerializeField] public float select_hint_bump_speed = 24f;
         // [SerializeField] public float arg_help_bump_offset_amount = -6f;
         // [SerializeField] public float arg_help_bump_speed = 8f;
         // [SerializeField] public float arg_help_width_padding = 8f;
